cf4e07bc8fccddf88166dbb452dd03513d7a9dca
github username <github email address>
2019-11-11 23:06:00 -0800

    updated seeds and added seed package

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## Travelat'r Backend
 
 - To get the server going, run `nodemon index` in the terminal.
-- To get the database going, run any of the followng in the temrinal:
+- To get the database going, run any of the followng in the terminal:
     1. `mongod --config /usr/local/etc/mongod.conf`
     2. `brew services start mongodb-community@4.2`
 
@@ -23,3 +23,19 @@
         - stats => object (demographics & such)
 
 - Auth for `Node.js`
+- Validation 
+
+
+
+STEPS FOR TESTING DATABASE
+
+1. Start `node idex.js` to start port 8080 ==> node
+2. Start `mongod` to start 127...travelatr route ==> mongod
+3. Start `mongo` last ==> mongo
+4. Open MongoDB Compass (needs to be downloaded first) to check what's in the local database.
+5. Test database manipulations using the following:
+ - type `db`
+ - type `use travelatr`
+ - type `db.users.insertOne({username: "becca"})`
+ - type `db.users.find()`
+ - check the Compass table to see if there are any database changes.
--- seeds/seeds.js
+++ seeds/seeds.js
@@ -6,6 +6,11 @@ User = require("../models/userModel.js");
 
 var expenses = require("./expenseData.json");
 
+// need to make an object of countries to use to iterate over for country creation
+// const locations = [
+//     FTW, 
+// ]
+
 // for (let i = 0; i < 15; i++) {
 //     console.log(expenses["expenses"][i]["Value"]);
 // }
@@ -19,9 +24,9 @@ var expenses = require("./expenseData.json");
 // let user = new User();
 // console.log(user);
 
-function createUserExpenses(expenses) {
-    console.log(expenses["expenses"][0]);
-    let user = new User();
+let user;
+function createUser() {
+    user = new User();
     user.username = "carnie";
     user.email = "carnie@example.com";
     user.password = "password";
@@ -30,4 +35,30 @@ function createUserExpenses(expenses) {
     console.log(user);
 }
 
+let country;
+function createCountry() {
+    country = new Country();
+    country.name = "hungary";
+    country.stats = "rando";
+    country.polyglonCoordinates = "XYZ";
+    country.save();
+    console.log(country);
+}
+
+let expense; 
+function createUserExpenses(expenses) {
+    console.log(expenses["expenses"][0]);
+    createUser();
+    createCountry();
+    expense = new Expense();
+    expense.userId = user.id;
+    expense.countryId = country.id;
+    expense.expenseType = expenses["expenses"][0].category;
+    expense.value = expenses["expenses"][0].value;
+    expense.save();
+    console.log(expense);
+}
+
 createUserExpenses(expenses);
+
+console.log(db.users.find())
